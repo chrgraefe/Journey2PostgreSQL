f04de1f6ab23c19a379803bf713905160b6ae76a
Christian Gräfe <Christian.Graefe@deutschebahn.com>
2025-02-10 11:09:58 +0100

    1. Seite deployed

---
+++ content/first-chapter/first-page/_index.md
@@ -0,0 +1,7 @@
++++
+date = '2025-02-10T11:05:32+01:00'
+draft = false
+title = 'Data Types'
++++
+
+Comparions of data type
---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Comparing SQL Server with PostgreSQL in different ways",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "Comparisons",
+    "uri": "/first-chapter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Comparing SQL Server with PostgreSQL in different ways",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Comparing SQL Server with PostgreSQL in different ways \u003e Comparisons",
+    "content": "Comparions of data type",
+    "description": "Comparions of data type",
+    "tags": [],
+    "title": "Comparison of data types",
+    "uri": "/first-chapter/first-page/index.html"
+  },
+  {
+    "breadcrumb": "Comparing SQL Server with PostgreSQL in different ways",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Comparing SQL Server with PostgreSQL in different ways",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
